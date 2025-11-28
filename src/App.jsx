4c2b6776e62c286782fea1da62c058dcd7815fa6
Charniel Duterte <charniel.bposeats@gmail.com>
2025-11-28 19:50:22 +0800

    add emoji

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ export default function App() {
         type="text"
         value={text}
         onChange={handleChange}
-        placeholder="Yasminify your text here!"
+        placeholder="Yasminify your text here! 🌸"
         style={styles.input}
       />
 
